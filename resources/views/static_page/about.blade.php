49134adcfb4e65d534450aedc356bbaddadab4a0
kevin <404942221@qq.com>
2019-11-21 16:15:07 +0800

    基础页面++

---
+++ resources/views/static_page/about.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.default")
+@section("title", "这是关于页")
+@section("content")
+  <h1>关于</h1>
+@endsection
---
+++ resources/views/static_page/help.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.default")
+@section("title", "这是帮助页")
+@section("content")
+  <h1>帮助</h1>
+@endsection
